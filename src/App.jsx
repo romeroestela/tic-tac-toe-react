281b23ca0fad6d8f5154d8797c9d3bec8b42eed6
estela.romero.edu <romeroestela3@gmail.com>
2024-04-15 17:14:43 +0200

    Implementar lógica básica del juego

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,135 @@
 import './App.css'
+import { useState } from 'react'
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
 
-function App() {
+
+
+const Square = ({children, isSelected, updateBoard, index}) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
   
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+}
+
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+const MensajeWinner = ({winner}) => {
 
   return (
-    <>
-      <p>Hola Mundo</p>
-    </>
+    <div>
+      <p>¡Tenemos un ganador! ¡{winner}  eres el ganador!</p>
+    </div>
+  )
+}
+
+const MensajeEmpate = () => {
+
+  return (
+    <div>
+      <p>¡Tenemos un empate!</p>
+    </div>
+  )
+}
+
+function App() {
+
+  const [board, setBoard] = useState(
+    Array(9).fill(null)
+  )
+
+  const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null) // null no hay ganador, false hay empate
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS ){
+      const [a, b, c] = combo
+      if (boardToCheck[a] &&
+      boardToCheck[a] == boardToCheck[b] &&
+      boardToCheck[b] == boardToCheck[c])
+      {
+        return boardToCheck[a] 
+      }
+    }
+    //Si no hay ganador
+    return null
+  }
+
+  const updateBoard = (index) => {
+    console.log(winner)
+    if (board[index] || winner) return //Si ya tiene algo no se actualiza el tablero
+    //Actualiza el tablero
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    //Cambiar turnos
+    const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X 
+    setTurn(newTurn)
+
+    const newWinner = checkWinner(newBoard)
+    if(newWinner) {
+      setWinner(newWinner)
+      MensajeWinner(newWinner)
+    }
+  }
+
+  return (
+    <main className='board'>
+      <h1>Tic Tac Toe</h1>
+      <section className='game'>
+        {
+          board.map((_, index) => {
+            return (
+              <Square 
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section>
+      {
+        winner && (
+          <section className='winner'>
+          <MensajeWinner 
+            winner={winner}
+          >
+          </MensajeWinner>
+      </section>
+        ) 
+      }
+      
+      <section className='turn'>
+        <Square isSelected={turn == TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn == TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+    </main>
+    
   )
 }
 
